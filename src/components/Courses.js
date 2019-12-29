82a2d8f6d108f6305fd8f6e429de8f6c97f51431
Jhill161 <Jason161@gmail.com>
2019-12-29 13:50:42 -0800

    First Commit

---
+++ src/components/Courses.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
+import JavaScript from './courses/JavaScript';
+
+
+const Courses = ({match}) => (
+  <div className="main-content courses">
+    <div className="course-header group">
+      <h2>Courses</h2> 
+      <ul className="course-nav">
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
+      </ul>
+    </div>
+    
+    
+    <Route exactpath="/courses" render={ () => <Redirect to="/courses/html" /> } />
+    <Route path="/courses/html" component={HTML} />
+    <Route path="/courses/css" component={CSS} />
+    <Route path="/courses/javascript" component={JavaScript} />
+  </div>
+);
+
+export default Courses;
